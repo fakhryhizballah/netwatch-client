3e9f7500d1999932692773a6e36699ab7830ae7d
fakhryhizballah <falehry88@gmail.com>
2024-07-08 15:04:46 +0700

    feat: Add client.js and client2.js for MQTT client connection
    This commit adds two new files, client.js and client2.js, which are responsible for establishing a connection with an MQTT broker. The files use the dotenv package to load environment variables for the MQTT host, port, username, password, and client ID. The client.js file connects to the broker using the provided credentials and publishes a "client/Online" message with the client ID. It also subscribes to the "gateway/{client ID}" topic to receive messages from the gateway. When a message is received, the file checks the status of each member in the client group by pinging their IP addresses. If a member is online, it publishes an "client/Update/online" message with the member's ID. If a member is offline, it publishes an "client/Update/offline" message with the member's ID. The client2.js file follows the same logic as client.js but uses a different client ID.
    
    The commit also includes a new "env" file that contains environment variables for the MQTT configuration. These variables include the MQTT host, port, username, password, client IDs, time zone, and interval for publishing the "client/Online" message.

---
+++ clinet.js
@@ -0,0 +1,50 @@
+require('dotenv').config();
+const mqtt = require("mqtt");
+const ping = require("ping");
+const client = mqtt.connect({
+    host: process.env.MqttHost,
+    port: process.env.MqttPort,
+    username: process.env.MqttUsername,
+    password: process.env.MqttPassword,
+    clientId: process.env.MqttClientId,
+    keepalive: 60,
+    protocolId: 'MQTT',
+    protocolVersion: 4,
+    will: {
+        topic: 'clinet/Offline',
+        payload: process.env.MqttClientId,
+        qos: 1,
+        retain: false
+    }
+});
+console.log(process.env.MqttClientId);
+client.on("connect", () => {
+    console.log("connected");
+    client.publish("clinet/Online", process.env.MqttClientId)
+    client.subscribe("gateway/" + process.env.MqttClientId)
+});
+
+client.on("message", (topic, message) => {
+    // message is Buffer
+    // console.log(message.toString());
+    if (topic == "gateway/" + process.env.MqttClientId) {
+        console.log("client Ping");
+        // client.publish("gateway/" + message, 'ini adalah pesan dari server');
+        let clientGroup = JSON.parse(message.toString());
+        for (let e of clientGroup.members) {
+            ping.sys.probe(e.ip, function (isAlive) {
+                var msg = isAlive ? 'host ' + e.ip + ' is alive' : 'host ' + e.ip + ' is dead';
+                console.log(msg);
+                if (isAlive) {
+                    client.publish("cilent/Update/online", e.id.toString());
+                } else {
+                    client.publish("cilent/Update/offline", e.id.toString());
+                }
+            }, { timeout: 10, extra: ['-i', '2'], });
+        }
+    }
+});
+setInterval(function () {
+    //code goes here that will be run every 5 seconds.    
+    client.publish("clinet/Online", process.env.MqttClientId)
+}, process.env.setInterval);
